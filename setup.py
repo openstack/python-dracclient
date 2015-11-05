ef3d9a292e8ac7358aa3bf02f57cc8bde3e4598a
Imre Farkas <ifarkas@redhat.com>
2015-11-05 17:27:35 +0100

    Switch to PBR
    Change-Id: I7f5388aa306a32cdd954575c9c1942ebb63937b4

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
-import re
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-from setuptools import setup
+import setuptools
 
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-with open('requirements.txt', 'r') as fp:
-    install_requires = [re.split(r'[<>=]', line)[0]
-                        for line in fp if line.strip()]
-
-
-setup(
-    name = 'python-dracclient',
-    version = '0.0.3',
-    description = 'Library for managing machines with Dell iDRAC cards.',
-    author = 'Imre Farkas',
-    author_email = 'ifarkas@redhat.com',
-    url = 'https://github.com/ifarkas/python-dracclient',
-    packages = ['dracclient'],
-    install_requires = install_requires,
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Environment :: OpenStack',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7'
-    ],
-    license = 'APL 2.0',
-)
+setuptools.setup(
+    setup_requires=['pbr>=1.8'],
+    pbr=True)
